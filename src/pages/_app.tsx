81593b650852e4a5901f7891da6f72779d99da2b
João <jp.professional@outlook.com>
2021-05-12 10:41:21 -0300

    refactor: remove unused styles and add typescript

---
+++ src/pages/_app.tsx
@@ -0,0 +1,13 @@
+import GlobalStyles from '../styles/global'
+
+function MyApp({ Component, pageProps }) {
+  return (
+   <>
+      <GlobalStyles />
+      <Component {...pageProps} />
+   </>
+  
+  )
+}
+
+export default MyApp
