4c7357641616fd6e875cfbbd9e514c672eae04cb
João <jp.professional@outlook.com>
2021-05-12 11:21:11 -0300

    feat: create product page

---
+++ src/pages/[product]/p.tsx
@@ -0,0 +1,9 @@
+import * as S from '../../styles/productPage'
+
+export default function ProductPage(){
+    return (
+        <S.Wrapper>
+            <h1>Página de produto</h1>
+        </S.Wrapper>
+    )
+}
--- src/pages/route3.tsx
+++
@@ -1,3 +0,0 @@
-export default function Home() {
-  return <h1>Rota 3</h1>
-}
